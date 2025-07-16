a4cd8082977ae3a0ff7f542a450cf8d50dc9ea3f
Wasilat Amusa <wasilat@Mac.lan>
2025-07-15 23:33:22 -0400

    Update API function

--- anagram_api.py
+++ anagram_api.py
@@ -1,9 +1,30 @@
 import requests
 
-#API I got from RapidAPI
-api_host = "word-checker-api.p.rapidapi.com"
+#API credentials from RapidAPI
+api_host = "x-rapidapi-host: word-checker-api.p.rapidapi.com"
 api_key = "c79c515bb3msh30d268472db25dap129bb0jsndee7029ec1f8"
 
-#url-- https://word-checker-api.p.rapidapi.com/v1/tools/anagram-solver/HI 
-#web url -- https://rapidapi.com/wordcheckerio/api/word-checker-api/playground/apiendpoint_41a05fbd-7068-4502-a78a-dacfd2de93dd
-def get_words():
+#function to get anagrams for a given word using the Word Checker API
+def get_words(word):
+    url = f"https://word-checker-api.p.rapidapi.com/v1/tools/anagram-solver/{word}"
+
+    #required headers for RapidAPI authentication
+    headers = {
+        "x-rapidapi-host": api_host,
+        "x-rapidapi-key": api_key
+    }
+
+    #send a get request to the API
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        #gets list of anagrams from API response
+        anagrams = data.get("anagrams", [])
+        return anagrams
+    else:
+        #prints error if the request fails and return an empty list
+        print(f"API error {response.status_code}")
+        return []
+
+
