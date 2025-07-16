79ef7fa31eb77a100c4f4c27ea8375804f67a10d
Wasilat Amusa <wasilat@Mac.lan>
2025-07-15 21:36:41 -0400

    Found API for words

---
+++ anagram_api.py
@@ -0,0 +1,8 @@
+import requests
+
+#API I got from RapidAPI
+api_host = "word-checker-api.p.rapidapi.com"
+api_key = "c79c515bb3msh30d268472db25dap129bb0jsndee7029ec1f8"
+
+#url-- 
+def get_words():
