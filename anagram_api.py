f8d7e994bb467d9cd4f65f3567430ecf3c2f4cda
Wasilat Amusa <wasilat@Mac.lan>
2025-07-15 21:50:57 -0400

    Slight update to file

--- anagram_api.py
+++ anagram_api.py
@@ -4,5 +4,6 @@ import requests
 api_host = "word-checker-api.p.rapidapi.com"
 api_key = "c79c515bb3msh30d268472db25dap129bb0jsndee7029ec1f8"
 
-#url-- 
+#url-- https://word-checker-api.p.rapidapi.com/v1/tools/anagram-solver/HI 
+#web url -- https://rapidapi.com/wordcheckerio/api/word-checker-api/playground/apiendpoint_41a05fbd-7068-4502-a78a-dacfd2de93dd
 def get_words():
