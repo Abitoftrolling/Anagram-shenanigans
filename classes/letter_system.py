747dbe4083d69a1ebbedddedcba62d7017bc4c66
Wasilat Amusa <wasilat@Mac.lan>
2025-07-24 16:51:10 -0400

    Updated letter system file + words.txt

--- classes/letter_system.py
+++ classes/letter_system.py
@@ -1,3 +1,18 @@
 import random 
-import string
-#to be continued...
+
+#function to load words from a text file 
+def load_words():
+    words = [] #empty list to store words 
+    with open("classes/words.text", "r") as file: #opens words.txt file in classes folder
+        for f in file: #reads each line in the file 
+            word = f.strip() #removes any extra spaces or newline chars 
+            if 3 <= len(word) <= 8: #keeps words between 3 and 8 letters long
+                words.append(word) #adds the words to list
+    return words
+
+#functon to pick a random word and shuffle its letters 
+def get_letters(words):
+    word = random.choice(words) #picks the random word from list
+    letters = list(word)  #converts word into a list of letters
+    random.shuffle(letters)  #shuffles the letters randomly
+    return letters, word  #returns the shuffles letters and the og word 
